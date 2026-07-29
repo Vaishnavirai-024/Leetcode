af73974393a184e101d05abe598dd375e43260e8
Vaishnavi Rai <vaishnavirai024@gmail.com>
2026-07-29 23:06:37 +0530

    Time: 4 ms (73.83%) | Memory: 45.6 MB (11.11%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,24 +1,46 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       int n1=nums1.length;
-       int n2=nums2.length;
-       int ans[]=new int[n1];
-       for(int i=0;i<n1;i++){
-        ans[i]=-1;
-        int idx=-1;
-        for(int j=0;j<n2;j++){
-            if(nums2[j]==nums1[i]){
-                idx=j;
-                break;
+
+        int n1=nums1.length;
+        int n2=nums2.length;
+        Stack<Integer>st=new Stack<>();
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums2[i]>st.peek()){
+                st.pop();
+            }if(st.isEmpty()){
+                map.put(nums2[i],-1);
+            }else {
+                map.put(nums2[i],st.peek());
             }
+            st.push(nums2[i]);
         }
-        for(int j=idx+1;j<n2;j++){
-            if(nums2[j]>nums1[i]){
-                ans[i]=nums2[j];
-                break;
-            }
+        int ans[]=new int[n1];
+        for(int i=0;i<n1;i++){
+            ans[i]=map.get(nums1[i]);
         }
-       }
-       return ans;
+        return ans;
     }
 }
+    //    int n1=nums1.length;
+    //    int n2=nums2.length;
+    //    int ans[]=new int[n1];
+    //    for(int i=0;i<n1;i++){
+    //     ans[i]=-1;
+    //     int idx=-1;
+    //     for(int j=0;j<n2;j++){
+    //         if(nums2[j]==nums1[i]){
+    //             idx=j;
+    //             break;
+    //         }
+    //     }
+    //     for(int j=idx+1;j<n2;j++){
+    //         if(nums2[j]>nums1[i]){
+    //             ans[i]=nums2[j];
+    //             break;
+    //         }
+    //     }
+    //    }
+    //    return ans; 
+//     }
+// }
